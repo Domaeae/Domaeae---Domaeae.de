5b3de4009c92b97ed1531ef7a031c1d36e19f59b
Domaeae <DominikGirrbach@live.de>
2022-07-08 17:22:47 +0200

    - Darkmode deaktiviert bis fertig WIP:  Font von local auf CDN

--- src/js/app.js
+++ src/js/app.js
@@ -1,40 +1,46 @@
 $(function () {
 
-  // function isDark(color) {
-  //   var match = /rgb\((\d+).*?(\d+).*?(\d+)\)/.exec(color);
-  //   return parseFloat(match[1])
-  //     + parseFloat(match[2])
-  //     + parseFloat(match[3])
-  //     < 3 * 256 / 2; // r+g+b should be less than half of max (3 * 256)
-  // }
 
+
+  // Variablen
 
   const app = $('.app'); // Ganze App (HTML fällt noch)
   const app_body = $('.app-body'); //innere teil
-
   const isDarkMode =
     window.matchMedia &&
     window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 
+
+  // IF-Schleifen
+
   if (isDarkMode) {
-
-    console.log('Dark modefo is enabled');
-    $('body').addClass('dark');
-    app_body.addClass('dark-body');
-
-
-    // Dark-Buttons verdunkeln
-    $('.btn-md-outline-dark').addClass('btn-md-primary');
-    $('.btn-md-outline-dark').removeClass('btn-md-outline-dark');
-
-    $('.btn-back').addClass('btn-md-pink').removeClass('btn-md-outline-dark');
-    // $('.btn-d-outline-dark').removeClass('btn-md-outline-dark');
-
-
-
+    // Enabled when Optimize
+    // enable_darkmode();
   } else {
     console.log('Light mode is enabled');
   };
 
+
+
+  // Funktionen
+
+  function enable_darkmode() {
+    $('body').addClass('dark');
+    app_body.addClass('dark-body');
+    // Dark-Buttons verdunkeln
+    $('.btn-md-outline-dark').addClass('btn-md-primary');
+    $('.btn-md-outline-dark').removeClass('btn-md-outline-dark');
+    $('.btn-back').addClass('btn-md-pink').removeClass('btn-md-outline-dark');
+    // $('.btn-d-outline-dark').removeClass('btn-md-outline-dark');
+  }
+
 });
+
+// function isDark(color) {
+//   var match = /rgb\((\d+).*?(\d+).*?(\d+)\)/.exec(color);
+//   return parseFloat(match[1])
+//     + parseFloat(match[2])
+//     + parseFloat(match[3])
+//     < 3 * 256 / 2; // r+g+b should be less than half of max (3 * 256)
+// }
---
+++ todo.md
@@ -0,0 +1,5 @@
+** ToDo:**
+
+- Projekt Seite - Gallerie mit filter: 
+  - - https://api.jquery.com/filter/
+  - https://placeimg.com/
